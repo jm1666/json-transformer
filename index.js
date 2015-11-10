d8713862e516724a45025e1795a330774534e765
Lawrence Law <d201.c317@gmail.com>
2015-11-10 19:46:59 +0800

    Reformatted, add simple filtering iteration

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ var SpellChecker = require('hunspell-spellchecker');
 var start = new Date();
 
 program
-    .version('1.0.0')
-    .option('-p, --pretty', 'Pretty Formatted JSON in the outputs', false)
-    .option('-m, --metadata', 'Generate Metadata', false)
-    .option('-s, --spell', 'Enable Spell Checker', false)
-    .arguments('<cmd> <dictionaryPath>')
-    .action(function (cmd, dictionaryPath) {
-        cmdValue = cmd;
-        dictionaryPathValue = dictionaryPath;
-    });
+  .version('1.0.0')
+  .option('-p, --pretty', 'Pretty Formatted JSON in the outputs', false)
+  .option('-m, --metadata', 'Generate Metadata', false)
+  .option('-s, --spell', 'Enable Spell Checker', false)
+  .arguments('<cmd> <dictionaryPath>')
+  .action(function (cmd, dictionaryPath) {
+    cmdValue = cmd;
+    dictionaryPathValue = dictionaryPath;
+  });
 
 program.parse(process.argv);
 
@@ -30,17 +30,17 @@ var spellchecker_UK = new SpellChecker();
  */
 function InitDic(callback) {
 
-    var DICT_US = spellchecker_US.parse({
-        aff: fs.readFileSync(dictionaryPathValue + "/en_US.aff"),
-        dic: fs.readFileSync(dictionaryPathValue + "/en_US.dic")
-    });
+  var DICT_US = spellchecker_US.parse({
+    aff: fs.readFileSync(dictionaryPathValue + "/en_US.aff"),
+    dic: fs.readFileSync(dictionaryPathValue + "/en_US.dic")
+  });
 
-    var DICT_UK = spellchecker_UK.parse({
-        aff: fs.readFileSync(dictionaryPathValue + "/en_GB.aff"),
-        dic: fs.readFileSync(dictionaryPathValue + "/en_GB.dic")
-    });
+  var DICT_UK = spellchecker_UK.parse({
+    aff: fs.readFileSync(dictionaryPathValue + "/en_GB.aff"),
+    dic: fs.readFileSync(dictionaryPathValue + "/en_GB.dic")
+  });
 
-    callback && callback(DICT_US, DICT_UK);
+  callback && callback(DICT_US, DICT_UK);
 }
 // End Init Dictionary Buffer
 
@@ -49,17 +49,17 @@ function InitDic(callback) {
  * @param callback   Result Exported For Downstream Processing
  */
 function readfile(callback) {
-    fs.readFile(cmdValue, 'utf8', function (err, data) {
-        if (err) {
-            callback && callback(err);
-        } else {
-            var dir = './output';
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
-            callback && callback(data);
-        }
-    });
+  fs.readFile(cmdValue, 'utf8', function (err, data) {
+    if (err) {
+      callback && callback(err);
+    } else {
+      var dir = './output';
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      callback && callback(data);
+    }
+  });
 }
 
 /**
@@ -70,48 +70,65 @@ function readfile(callback) {
  * @param callback  Output
  */
 function parseFile(entry, DICT_US, DICT_UK, callback) {
-    if (JSON.parse(JSON.stringify(entry)).errno) {
-        console.error(JSON.parse(JSON.stringify(entry)).code);
-        process.exit(1);
+  if (JSON.parse(JSON.stringify(entry)).errno) {
+    console.error(JSON.parse(JSON.stringify(entry)).code);
+    process.exit(1);
+  } else {
+    var data = JSON.parse(entry);
+
+    if (program.spell) {
+      spellchecker_US.use(DICT_US);
+      spellchecker_UK.use(DICT_UK);
+      var checking = _.map(data, function (d, k) {
+        if (k.match(/^[a-zA-Z]+$/) && k.length > 1 && spellchecker_US.check(k) || spellchecker_UK.check(k)) {
+          return {
+            lemma: k,
+            exist: _.map(d, function (e) {
+              return {
+                path: e
+              }
+            })
+          }
+        } else {
+          return false;
+        }
+      });
+
+      var rejecting = _.reject(checking, function (d) {
+        return d == false
+      });
+
+      callback && callback(rejecting);
     } else {
-        var data = JSON.parse(entry);
-
-        if (program.spell) {
-            spellchecker_US.use(DICT_US);
-            spellchecker_UK.use(DICT_UK);
-            var L1checked = _.map(data, function (d, k) {
-                if (k.match(/^[a-zA-Z]+$/) && k.length > 1 && spellchecker_US.check(k) || spellchecker_UK.check(k)) {
-                    return {
-                        lemma: k,
-                        exist: _.map(d, function (e) {
-                            return {
-                                path: e
-                            }
-                        })
-                    }
-                } else {
-                    return false;
-                }
-            });
-
-            var L2filtering = _.reject(L1checked, function (d) {
-                return d == false
-            });
-
-            callback && callback(JSON.stringify(L2filtering, null, 2));
+      var checked = _.map(data, function (d, k) {
+        if (k.match(/^[a-zA-Z]+$/) && k.length > 1) {
+          return {
+            lemma: k,
+            exist: _.map(d, function (e) {
+              return {
+                path: e
+              }
+            })
+          }
+        } else {
+          return false;
         }
+      });
 
-        if (!(program.spell)) {
-            callback && callback(JSON.stringify(data, null, 2));
-        }
+      var rejected = _.reject(checked, function (d) {
+        return d == false
+      });
+
+      callback && callback(rejected);
     }
+  }
 }
 
 
 InitDic(function (DICT_US, DICT_UK) {
-    readfile(function (result) {
-        parseFile(result, DICT_US, DICT_UK, function (callback) {
-            console.log(callback);
-        })
+  readfile(function (result) {
+    parseFile(result, DICT_US, DICT_UK, function (callback) {
+      console.log(JSON.stringify(callback, null, 2));
     })
+  })
 });
